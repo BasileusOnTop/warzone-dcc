f6770dec906a837da59fd7457809acba5f39d3f1
Daniele Bartolini <danyatk@gmail.com>
2013-07-05 15:52:03 +0200

    Add warzone dir

---
+++ samples/warzone/main.cpp
@@ -0,0 +1,46 @@
+#include "Crown.h"
+
+using namespace crown;
+
+//-----------------------------------------------------------------------------
+void init()
+{
+}
+
+//-----------------------------------------------------------------------------
+void frame(float dt)
+{
+	if (device()->keyboard()->key_pressed(KC_ESCAPE))
+	{
+		device()->stop();
+	}
+}
+
+//-----------------------------------------------------------------------------
+void shutdown()
+{
+
+}
+
+//-----------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+	crown::os::init_os();
+
+	crown::Device* engine = crown::device();
+
+	if (!engine->init(argc, argv, init))
+	{
+		exit(-1);
+	}
+
+	// Main loop
+	while (engine->is_running())
+	{
+		engine->frame(frame);
+	}
+
+	engine->shutdown(shutdown);
+
+	return 0;
+}
