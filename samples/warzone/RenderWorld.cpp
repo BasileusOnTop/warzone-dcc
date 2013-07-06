842b21e978693cc371f4a22c22296ebda9277846
Daniele Bartolini <danyatk@gmail.com>
2013-07-06 18:54:24 +0200

    Render agents by type

--- samples/warzone/RenderWorld.cpp
+++ samples/warzone/RenderWorld.cpp
@@ -27,29 +27,83 @@ void RenderWorld::frame()
 {
 	DebugRenderer* dr = device()->debug_renderer();
 
+	// Reset matrices to default
 	device()->renderer()->set_matrix(MT_MODEL, Mat4::IDENTITY);
 	device()->renderer()->set_matrix(MT_PROJECTION, m_projection);
 	device()->renderer()->set_matrix(MT_VIEW, Mat4::IDENTITY);
 
-	dr->add_line(Vec3(0, 0, 0), Vec3(15, 0, 15), Color4::RED, true);
-	dr->add_line(Vec3(0, 0, 0), Vec3(15, 15, 0), Color4::RED, true);
-	dr->add_line(Vec3(0, 0, 0), Vec3(0, 15, 15), Color4::RED, true);
-
-	//dr->add_sphere(Vec3(0, 0, -25), 50.0f, Color4::RED, true);
-
-	dr->add_hexagon(Vec3(50, 50, -1.0f), 16.0f, Color4::RED, true);
-
-	for (uint32_t i = 0; i < AGENTS; i++)
-	{
-		const float& x = m_env->m_agents[i]->m_positionX;
-		const float& y = m_env->m_agents[i]->m_positionY;
-
-		dr->add_hexagon(Vec3(x, y, -1.0f), 16.0f, Color4::RED, true);
-	}
+	// Render all the agents, dynamically selecting the shape of each
+	render_agents();
 }
 
 //-----------------------------------------------------------------------------
 void RenderWorld::shutdown()
 {
 
+}
+
+//-----------------------------------------------------------------------------
+void RenderWorld::render_agents()
+{
+	for (uint32_t i = 0; i < AGENTS; i++)
+	{
+		Agent* ith = m_env->m_agents[i];
+
+		// FIXME FIXME FIXME
+		// if (ith->is_dead())
+		// {
+		// 	continue;
+		// }
+
+		switch (ith->get_type())
+		{
+			case HQ:
+			{
+				render_hq(ith);
+				break;
+			}
+			case TURRET:
+			{
+				render_turret(ith);
+				break;
+			}
+			case UNIT:
+			{
+				render_unit(ith);
+				break;
+			}
+			case NONE:
+			default:
+			{
+				break;
+			}
+		}
+	}
+}
+
+//-----------------------------------------------------------------------------
+void RenderWorld::render_hq(Agent* agent)
+{
+}
+
+//-----------------------------------------------------------------------------
+void RenderWorld::render_turret(Agent* agent)
+{
+	DebugRenderer* dr = device()->debug_renderer();
+
+	const float& x = agent->m_positionX;
+	const float& y = agent->m_positionY;
+
+	dr->add_hexagon(Vec3(x, y, -1.0f), 25.0f, Color4::RED, true);
+}
+
+//-----------------------------------------------------------------------------
+void RenderWorld::render_unit(Agent* agent)
+{
+	DebugRenderer* dr = device()->debug_renderer();
+
+	const float& x = agent->m_positionX;
+	const float& y = agent->m_positionY;
+
+	dr->add_circle(Vec3(x, y, -1.0f), 16.0f, Color4::RED, true);	
 }
--- samples/warzone/RenderWorld.h
+++ samples/warzone/RenderWorld.h
@@ -4,18 +4,25 @@
 using namespace crown;
 
 class Environment;
+class Agent;
 
 class RenderWorld
 {
 public:
 
-	RenderWorld(Environment* env);
+					RenderWorld(Environment* env);
 
-	void	init();
-	void	frame();
-	void	shutdown();
+	void			init();
+	void			frame();
+	void			shutdown();
+
+	void			render_agents();
 
 private:
+
+	void			render_hq(Agent* agent);
+	void			render_turret(Agent* agent);
+	void			render_unit(Agent* agent);
 
 	Environment*	m_env;
 	Mat4			m_projection;
