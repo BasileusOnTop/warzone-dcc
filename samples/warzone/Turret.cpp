4b1ee70207ac1a233ef86198fc79bb305cd2c23c
BasileusOnTop <alessio.bianchetti@gmail.com>
2013-07-07 10:02:09 +0200

    added Turret class skel

---
+++ samples/warzone/Turret.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "Turret.h"
+#include "Environment.h"
+
+Turret::Turret(uint32_t health, uint32_t damage, float coord_x, float coord_y, float radius, Factions team, Environment* env) 
+	:Agent(health, damage, coord_x, coord_y, radius, team, env)
+{
+	m_speed 	= 0.0f;
+	m_type 		= TURRET; //THIS IS OK
+}
