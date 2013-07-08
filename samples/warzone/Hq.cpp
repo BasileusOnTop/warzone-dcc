1f4ec2688989e052f1b78d4b2c68310bf38dbf89
BasileusOnTop <alessio.bianchetti@gmail.com>
2013-07-08 15:14:19 +0200

    added Headquarter agent

---
+++ samples/warzone/Hq.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "Hq.h"
+#include "Environment.h"
+
+Hq::Hq(const Vec2& pos, Faction team, Environment* env) 
+	:Agent(pos, team, env)
+{
+	m_health 	= 5000;
+	m_type 		= HQ;
+}
